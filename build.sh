307b5742652c308d00a40bc9819dd4af2106dbc7
yangzhiqi <yangzhiqi@baicm.com.cn>
2020-08-17 09:39:31 +0800

    upgrade go 1.15

--- build.sh
+++ build.sh
@@ -17,19 +17,18 @@ go env -w GOPROXY=https://goproxy.io,direct
 
 if [ "$1" = "mac" ]; then
     echo "Compile For Mac ..."
-    go build -gcflags "-m" -ldflags "-w" -o hadoop_exporter cmd/hadoop-exporter/hadoop_exporter.go && \
-        go build -gcflags "-m" -ldflags "-w" -o hbase_exporter cmd/hbase-exporter/hbase_exporter.go
+    go build -o hadoop_exporter cmd/hadoop-exporter/hadoop_exporter.go && \
+        go build -o hbase_exporter cmd/hbase-exporter/hbase_exporter.go
     echo 
-    echo " Compiled For Mac Done !"
+    echo "Compiled For Mac Done !"
 elif [ "$1" = "linux" ]; then
     echo "Compile For Linux ..."
-    GOOS=linux GOARCH=amd64 go build -gcflags "-m" -ldflags "-w" -o hadoop_exporter cmd/hadoop-exporter/hadoop_exporter.go && \
-        GOOS=linux GOARCH=amd64 go build -gcflags "-m" -ldflags "-w" -o hbase_exporter cmd/hbase-exporter/hbase_exporter.go
+    GOOS=linux GOARCH=amd64 go build -o hadoop_exporter cmd/hadoop-exporter/hadoop_exporter.go && \
+        GOOS=linux GOARCH=amd64 go build -o hbase_exporter cmd/hbase-exporter/hbase_exporter.go
     echo 
-    echo " Compiled For Linux Done !"
+    echo "Compiled For Linux Done !"
 elif [ "$1" = "rm" ]; then
     rm -f hadoop_exporter hbase_exporter
 else
     Usage
 fi
-
--- go.mod
+++ go.mod
@@ -1,10 +1,9 @@
 module github.com/zqyangchn/hadoop_exporter
 
-go 1.13
+go 1.15
 
 require (
 	github.com/prometheus/client_golang v1.7.1
-	github.com/prometheus/common v0.10.0
 	go.uber.org/zap v1.15.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
